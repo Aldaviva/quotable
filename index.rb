2d28b83e7c3aa91d8ecb69227a613c4c64dabbd6
Ben Hutchison <ben@aldaviva.com>
2013-03-02 03:14:38 -0800

    Styling. Form for submission.
    --HG--
    rename : views/styles/fonts.less => styles/fonts.less
    rename : views/styles/reset.less => styles/reset.less

--- index.rb
+++ index.rb
@@ -6,13 +6,13 @@ require 'less'
 
 include Mongo
 
-mongo_client = MongoClient.new("localhost", 27017)
+mongo_client = MongoClient.new("localhost", 27017, :j => true)
 db = mongo_client.db("quotable")
 collection = db.collection("quotations")
 
 def randomPaperName
-	@suffixes = %w{ post star gossip news times herald record advertiser dispatch gazette journal bugle review pioneer citizen standard bulletin clipper press sentinel extra voice tribune reporter report independent express beacon echo advocate statesman observer ledger clarion chronicle globe telegraph messenger sun examiner mail register enterprise }
-	@prefixes = %w{ new old daily weekly hometown local county national state tri-county valley district northern southern eastern western }.concat(@suffixes)
+	@suffixes = %w[ post star gossip news times herald record advertiser dispatch gazette journal bugle review pioneer citizen standard bulletin clipper press sentinel extra voice tribune reporter report independent express beacon echo advocate statesman observer ledger clarion chronicle globe telegraph messenger sun examiner mail register enterprise ]
+	@prefixes = %w[ new old daily weekly hometown local county national state tri-county valley district northern southern eastern western ].concat(@suffixes)
 	@name = "The "+@prefixes.sample+" "+@suffixes.sample(Random::rand(2)+1).join('-')
 	@name.gsub(/(?:[^a-zA-Z])([a-z])/) { |char| char.upcase }
 end
@@ -22,11 +22,19 @@ configure do
 end
 
 get '/' do
-	quotations = collection.find({}, { :sort => [[:date, Mongo::DESCENDING]] }).to_a
+	quotations = collection.find({}, { :sort => [[:date, Mongo::DESCENDING]], :limit => 100 }).to_a
 	erb :list, :locals => { :quotations => quotations }
 end
 
-get '/stylesheet.css' do
-	less :'styles/stylesheet'
+post '/' do
+	author = request["author"]
+	body   = request["body"]
+	if(!author.strip.empty? && !body.strip.empty?) then
+		collection.insert({ :body => body, :author => author, :date => Time.now })
+	end
+	redirect to('/')
 end
 
+get '/stylesheet.css' do
+	less :stylesheet
+end
---
+++ public/media/scripts/list.js
@@ -0,0 +1,42 @@
+(function(){
+
+	var form = $$("form.addQuote")[0];
+
+	function revealForm(){
+		form.removeClass('hidden');
+	}
+
+	function onSubmitForm(){
+		var validationError = getValidationError();
+		if(validationError != null){
+			alert("Your quotation isn't ready to submit:\n"+validationError+'.');
+			return false;
+		}
+		//if the user didn't even enter anything, just hide the form? and tell the user what happened?
+		//maybe just let the form time out if it's open and not interacted with for a while?
+	}
+
+	function getValidationError(){
+		var body = form.body.value;
+		if(body.trim() == ''){
+			return 'Body is empty';
+		}
+
+		var author = form.author.value;
+		if(author.trim() == ''){
+			return 'Author is empty';
+		}
+
+		return null;
+	}
+
+	form.addEvent('submit', onSubmitForm);
+
+	var revealerLink = $$('form.addQuote .revealer');
+
+	revealerLink.addEvent('click', function(e){
+		revealForm();
+		return false;
+	});
+
+})();
