11e99df8539b5ec122d895117e2898b3f40b0d2c
Ben Hutchison <ben@aldaviva.com>
2013-06-07 00:24:04 -0600

    Cleaned up unused code

--- public/media/scripts/update.js
+++ public/media/scripts/update.js
@@ -1,19 +1,5 @@
 (function(){
 
-	/*var eventSource = new EventSource('api/quotes/eventstream');
-
-	eventSource.addEventListener('message', handleEvent, false);
-
-	eventSource.addEventListener('open', function(event) {
-		// console.log("Event Stream connected.");
-	}, false);
-
-	eventSource.addEventListener('error', function(event) {
-		if(event.eventPhase == EventSource.CLOSED){
-			// console.error("Event Stream disconnected.");
-		}
-	}, false);*/
-
 	function handleEvent(event){
 		try {
 			var data = JSON.parse(event.data);
